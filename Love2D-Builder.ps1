cfff0b6f92e86e2941f9adaafd6d4e4815960d3a
Richard Seddon <rseddoniii@gmail.com>
2024-06-06 16:56:19 -0500

    Adding a new check
    Adding a check for the main.lua file, updating output

--- Love2D-Builder.ps1
+++ Love2D-Builder.ps1
@@ -1,18 +1,29 @@
-Write-Output("-Love2D Project Builder-")
+$Prefix = "[Love2D Project Builder]"
+Write-Host("$Prefix Love2D Project Builder Starting") -ForegroundColor DarkGreen
 
 #grab project path
-$ProjectPath = Read-Host "Enter path of project to build: "
+Write-Host("$Prefix Enter path of project to build: ") -ForegroundColor DarkGreen
+$ProjectPath = Read-Host
+
+#check for main.lua
+$MainFile = $ProjectPath + "\main.lua"
+if (-Not (Test-Path -Path $MainFile)) {
+    Write-Host("$Prefix Error: No main.lua file found in project path. Exiting...") -ForegroundColor DarkGreen
+    exit
+}
 
 #grab Love2D path
 $LovePath = "C:\Program Files\LOVE"
 if (-Not (Test-Path -Path $LovePath)) {
-    $LovePath = Read-Host "Enter Love2D install path: "
+    Write-Host("$Prefix Enter Love2D install path: ") -ForegroundColor DarkGreen
+    $LovePath = Read-Host
 }
 
 #grab build name, create folder
-$Name = Read-Host "Enter build name: "
-$BuildPath = (Get-Item $ProjectPath).parent.FullName + "\Builds\" + $Name + "-" + (Get-Date -Format "yyyyMMddTHHmmssffff")
-Write-Output("Creating build folder [$BuildPath] ...")
+Write-Host("$Prefix Enter build name: ") -ForegroundColor DarkGreen
+$Name = Read-Host
+$BuildPath = (Get-Item $ProjectPath).parent.FullName + "\Builds\" + (Get-Date -Format "yyyyMMddTHHmmssffff")
+Write-Host("$Prefix Creating build folder [$BuildPath] ...") -ForegroundColor DarkGreen
 $BuildsFolderPath = (Get-Item $ProjectPath).parent.FullName + "\Builds"
 if (-Not (Test-Path -Path $BuildsFolderPath)) {
     $TempPath = Split-Path (Split-Path $BuildPath -Parent) -Leaf
@@ -22,7 +33,7 @@ $TempPath = Split-Path $BuildPath -Leaf
 New-Item -Path $BuildsFolderPath -Name $TempPath -ItemType "directory" | Out-Null
 
 #compress project files
-Write-Output("Creating project executable file...")
+Write-Host("$Prefix Creating project executable file...") -ForegroundColor DarkGreen
 $ZipFile = $BuildPath + "\" +  $Name + ".zip"
 $ZipSource = $ProjectPath + "\*"
 Compress-Archive -Path $ZipSource -DestinationPath $ZipFile
@@ -40,18 +51,20 @@ Get-Content $LoveExecutable,$LoveFile -Encoding Byte | Set-Content $Executable -
 Remove-Item $LoveFile
 
 #copy in dlls, license
-Write-Output("Copying dlls...")
+Write-Host("$Prefix Copying dlls...") -ForegroundColor DarkGreen
 Copy-Item -Path ($LovePath + "\*.dll") -Destination $BuildPath
 Copy-Item -Path ($LovePath + "\license.txt") -Destination $BuildPath
 
 #prompt to zip
-$confirmation = Read-Host "Create build zip? y/n"
+Write-Host("$Prefix Create build zip? y/n") -ForegroundColor DarkGreen
+$confirmation = Read-Host 
 if ($confirmation -eq 'y') {
     $ZipSource = $BuildPath + "\*"
-    Write-Output("Compressing build...")
+    Write-Host("$Prefix Compressing build...") -ForegroundColor DarkGreen
     Compress-Archive -Path $ZipSource -DestinationPath $ZipFile
 }
 
-Read-Host "Process complete. Press ENTER to open directory"
+Write-Host("$Prefix Process complete. Press ENTER to open directory") -ForegroundColor DarkGreen
+Read-Host
 Invoke-Item $BuildPath
 exit
